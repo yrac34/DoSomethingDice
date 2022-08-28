cce8cfd0a49f032e2945270c297468ed1320685b
yrac34 <amyrimchun@gmail.com>
2022-08-27 22:50:26 -0700

    Initial Commit

---
+++ DoSomething/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoSomething
+//
+//  Created by Amy Chun on 8/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DoSomething/DoSomethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoSomethingApp.swift
+//  DoSomething
+//
+//  Created by Amy Chun on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct DoSomethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
